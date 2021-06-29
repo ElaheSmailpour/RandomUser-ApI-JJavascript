365be19eab2610331773f8f4d381be9fe790359d
Elahesmailpour <eli_es87@yahoo.com>
2021-06-29 12:18:51 +0200

    randomUser

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Axios from "axios";
+import React, { useState } from "react";
+import Profile from "./Profile"
+import { v4 as uuidv4 } from "uuid";
 function App() {
+  const [query, setQuery] = useState("")
+  const [userprofile,setuserProfile]=useState([])
+  const url = `https://randomuser.me/api/?results=${query}`;
+
+  const getData = async () => {
+
+    const result = await Axios.get(url)
+    console.log("result=", result)
+    setQuery("");
+    setuserProfile(result.data.results)
+  }
+  const onsubmitform = (e) => {
+    e.preventDefault()
+    getData()
+  }
+  const onchangeInput = (e) => {
+    setQuery(e.target.value)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={onsubmitform}>
+        <input type="text" placeholder="search" onChange={onchangeInput} value={query} />
+        <button>Result</button>
+      </form>
+      {userprofile !==[]&& userprofile.map(profile=><Profile key={uuidv4()} profile={profile}/>)}
+
     </div>
   );
 }
---
+++ src/Profile.js
@@ -0,0 +1,8 @@
+const Profile=({Profile})=>{
+    return(
+        <div className="Profile">
+<h1>{Profile.name.first}</h1>
+        </div>
+    )
+}
+export default Profile
